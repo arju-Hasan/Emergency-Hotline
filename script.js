0c285bf896da888aee7581a4a1f06c6be4329698
Arju-Hasan <rajuahmeed698@gmail.com>
2025-08-27 03:55:30 +0600

    add history section only not js code

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
 // heart ++ button
-const heartBtns = document.querySelectorAll(".heart-btn");
-const heartCount = document.getElementById("heart-count");
-let heartCounter = 0;
+const hearts = document.querySelectorAll(".heart-btn");
+const heartCountElement = document.getElementById("heart-count");
 
-heartBtns.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        heartCounter++;
-        heartCount.textContent = heartCounter;
-    });
-});
- const heartBtn = document.querySelectorAll(".heart-btn");
+let heartCount = 0;
 
- heartBtn.forEach((btn) => {
-   btn.addEventListener("click", () => {
-     btn.classList.toggle("text-red-500");
-   });
- });
+for (const heart of hearts) {
+  heart.addEventListener("click", function () {
+    if (heart.classList.contains("fa-regular")) {
+      heart.classList.remove("fa-regular");
+      heart.classList.add("fa-solid", "text-red-500");
+      heartCount++;
+    } else {
+      heart.classList.remove("fa-solid", "text-red-500");
+      heart.classList.add("fa-regular");
+      heartCount--;
+    }
+
+    heartCountElement.innerText = heartCount;
+  });
+}
+// const heartBtns = document.querySelectorAll(".heart-btn");
+// const heartCount = document.getElementById("heart-count");
+// let heartCounter = 0;
+// 
+// heartBtns.forEach((btn) => {
+//     btn.addEventListener("click", () => {
+//         heartCounter++;
+//         heartCount.textContent = heartCounter;
+//     });
+// });
+//  const heartBtn = document.querySelectorAll(".heart-btn");
+// 
+//  heartBtn.forEach((btn) => {
+//    btn.addEventListener("click", () => {
+//      btn.classList.toggle("text-red-500");
+//    });
+//  });
 
 //  copy ++ button
 const copyBtns = document.querySelectorAll(".copy-button");
@@ -171,4 +191,36 @@ copyBtn.addEventListener("click", () => {
         .catch((err) => {
           console.error("Failed to copy: ", err);
         });
+    });
+// ============================================
+// const transactiondata = [];
+
+document
+  .getElementById("call-button-national")
+  .addEventListener("click", function () {
+    alert("calling 999");
+ 
+
+    const transactionSection = document.getElementById("newTransaction");
+    transactionSection.innerHTML = ""; // Clear previous transactions
+
+    for (data of transactiondata) {
+      const div = document.createElement("div");
+      div.innerHTML = `
+      <div class="bg-white p-3 rounded-2xl m-3 flex justify-between items-center">
+        <div class="flex items-center">
+          <div class="p-3 rounded-full bg-[#f4f5f7]">
+            <img src="./assets/wallet1.png" class="mx-auto" alt="">
+          </div>
+          <div class="ml-3">
+            <h1>${data.name}</h1>
+            <p>${data.Date}</p>
+          </div>
+        </div>
+        <div>
+         <i class="fa-solid fa-ellipsis-vertical"></i>
+      </div>`;
+      alert("calling 999");
+      transactionSection.appendChild(div);
+       };
     });
